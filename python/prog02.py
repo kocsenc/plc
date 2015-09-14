932909d40a508f670b9cafbc881dcd2a4af1c541
Kocsen <kocsenc@gmail.com>
2015-09-14 15:35:52 -0400

    Setup prog02 and questions

---
+++ python/prog02.py
@@ -0,0 +1,22 @@
+# Name: Kocsen Chung
+# Time spent: 4h
+# Collaborators: n/a
+# Programming assignment #2 - Python and Impcore
+
+
+# Exercise 3
+def sigma(m, n):
+    pass
+
+# Exercise 4
+
+# Exercise 5
+
+# Exercise 6
+
+# Exercise 7
+
+# Exercise 8
+def binary(m):
+    pass
+
