8845816306762e132204031cdc84d4539ef7c00d
Kocsen <kocsenc@gmail.com>
2015-09-14 16:58:32 -0400

    potato

--- python/prog02.py
+++ python/prog02.py
@@ -8,13 +8,37 @@
 def sigma(m, n):
     pass
 
-# Exercise 4
+# Exercise 4a
+def exp(m, n):
+    pass
+
+# Exercise 4b
+def log(m, n):
+    pass
 
 # Exercise 5
+def choose(n, k):
+    pass
 
 # Exercise 6
+def fib(m):
+    pass
 
-# Exercise 7
+# Exercise 7a
+def prime(n):
+    pass
+
+# Exercise 7b
+def nthprime(n):
+    pass
+
+# Exercise 7c
+def sumprimes(n):
+    pass
+
+# Exercise 7d
+def relprime(m, n):
+    pass
 
 # Exercise 8
 def binary(m):
